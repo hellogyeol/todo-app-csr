29ac37bc119821ebf6cbba6f2f055585ca928ab7
hellogyeol <hellogyeol@gmail.com>
2023-02-08 23:59:47 +0900

    server: 홈 접속 시 DB 데이터 조회 및 출력

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 - dotenv
 
 1. 서버가 HTML 파일을 반환하도록 express 코드 작성
+- 정적 파일 제공 설정 public
+
+1. mongodb 연결 코드 추가
+- .env로 api key 분리
+
+1. /todoList GET 요청 시 DB에 todoList 조회 후 반환
+
+1. HTML에서 비동기 요청으로 todoList 조회
+
--- app.js
+++ app.js
@@ -1,11 +1,55 @@
+/* mongodb */
+require('dotenv').config();
+const { MongoClient } = require("mongodb");
+const url = process.env.DB_URL;
+const client = new MongoClient(url);
+
+
+/* express */
 const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 첫 접속 시 HTML 템플릿 제공
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+app.get('/todoList', (req, res) => {
+  getTodoList();
+  async function getTodoList() {
+    await client.connect();
+    const col = client.db('csrDb').collection('csrCol');
+
+    col.insertMany([
+      {
+        id: String(Date.now()),
+        done: false,
+        content: 'content1'
+      },
+      {
+        id: String(Date.now()),
+        done: false,
+        content: 'content2'
+      },
+      {
+        id: String(Date.now()),
+        done: false,
+        content: 'content3'
+      }
+    ]);
+
+    const todoList = await col.find({}).toArray();
+    console.log(todoList);
+
+    res.send(todoList);
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`To-Do app listening on port ${port}`);
 });
